f187d5389ebfeaf4124f87a861e541a7de8c5f5d
Macbook <macbook@macbooknomacbook-puro.local>
2020-11-26 21:47:13 +0900

    Initial Commit

---
+++ CountUp1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CountUp1
+//
+//  Created by Macbook on 2020/11/26.
+//  Copyright © 2020年 Tabata Sorane. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
